93183e4ea145a73e05050d686b5a96a206551f16
adamparcher <adam.parcher@gmail.com>
2012-03-15 16:15:50 -0700

    skeleton RoR app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoapRestWrapper::Application.config.secret_token = 'ecadb343e3f4ccca89065740c3415182d5f2bcf7e43593259e63ea97810a5184772eec11e217d9c204c1d755abaa24f76be82a906ed758a09b15ac912d6a7580'
